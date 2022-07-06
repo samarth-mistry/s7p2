64e18d68a956f12c0a99dc46068d221533c991aa
Rohan Andharai <rohanandharia@gmail.com>
2022-07-06 12:30:52 +0530

    login according to types of user

--- automatedhotel/core/urls.py
+++ automatedhotel/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', views.logins, name='login'),
 
 ]
--- automatedhotel/core/views.py
+++ automatedhotel/core/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    #return HttpResponse('This Is Home Page Please Add it')
+    return render(request,'index.html')
+
 def dashboard(request):
     return render(request,'dashboard.html')
+
+def logins(request):
+    return render(request,'login.html')
