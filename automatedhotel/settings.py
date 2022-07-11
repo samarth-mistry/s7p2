2445c92b4791e15378332b1d9db1be44300e260b
Samarth Mistry <samarthmistry13@outlook.com>
2022-07-11 10:15:40 +0530

    Heroku changes

--- automatedhotel/settings.py
+++ automatedhotel/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS =  [ os.path.join(BASE_DIR,'static') ]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+import django_heroku
+django_heroku.settings(locals())
