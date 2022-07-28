030b681d55017ac7b345c18ce57e75d1ba60c449
Samarth Mistry <samarthmistry13@outlook.com>
2022-07-28 12:37:47 +0530

    Dashboard bug resolved.

--- core/forms.py
+++ core/forms.py
@@ -21,3 +21,13 @@ class AdminRegistration(forms.ModelForm):
             'email' : forms.EmailInput(attrs={'class':'form-control'}),
             'password' : forms.PasswordInput(render_value=True, attrs={'class':'form-control'}),
         }
+
+class AdminRegistration(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+        widgets = {
+            'username' : forms.TextInput(attrs={'class':'form-control'}),
+            'email' : forms.EmailInput(attrs={'class':'form-control'}),
+            'password' : forms.PasswordInput(render_value=True, attrs={'class':'form-control'}),
+        }
